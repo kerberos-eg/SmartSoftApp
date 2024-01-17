5e895180ca8dec02ce31c69a59ab19995ec0ce5e
kerberos-eg <smartsoft.teh.eg@gmail.com>
2024-01-17 03:02:16 +0200

    home changes office

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,37 @@ class smartsoftapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Smartsoftapp'),
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
-        body: Center(
-          child: Text('hussein ali'),
-        ),
-      ),
+          body: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+            child: Center(
+              child: Text('hussein ali'),
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Color.fromARGB(255, 224, 14, 189),
+            child: Center(
+              child: Text('hussein ali'),
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Color.fromARGB(255, 12, 235, 31),
+            child: Center(
+              child: Text('hussein ali'),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
